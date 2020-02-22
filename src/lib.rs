f66b1a7a54a30b4bcc797585149bd2f2961e1945
Fenhl <fenhl@fenhl.net>
2020-02-22 08:43:53 +0000

    Cargo init

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! A Rust client for the [twitch.tv Helix API](https://dev.twitch.tv/docs/api).
+
+#![deny(missing_docs, rust_2018_idioms, unused, unused_import_braces, unused_qualifications, warnings)]
