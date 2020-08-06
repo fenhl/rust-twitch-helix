fa542d36f2b60121c909e07e0ca688a8df56e3c7
Fenhl <fenhl@fenhl.net>
2020-08-06 12:37:27 +0000

    Handle missing cursor on last page

--- src/paginated.rs
+++ src/paginated.rs
@@ -14,8 +14,36 @@ use {
 };
 
 #[derive(Deserialize)]
+#[serde(from = "Option<String>")]
+enum Cursor {
+    Start,
+    At(String),
+    End
+}
+
+impl Cursor {
+    fn query(self) -> Option<Vec<(String, String)>> {
+        match self {
+            Cursor::Start => Some(Vec::default()),
+            Cursor::At(cursor) => Some(vec![(format!("after"), cursor)]),
+            Cursor::End => None // to break the loop
+        }
+    }
+}
+
+impl From<Option<String>> for Cursor {
+    fn from(cursor: Option<String>) -> Cursor {
+        if let Some(cursor) = cursor {
+            Cursor::At(cursor)
+        } else {
+            Cursor::End
+        }
+    }
+}
+
+#[derive(Deserialize)]
 struct PaginationInfo {
-    cursor: String
+    cursor: Cursor
 }
 
 #[derive(Deserialize)]
@@ -25,16 +53,21 @@ struct PaginatedResult<T> {
 }
 
 pub(crate) fn stream<'a, T: DeserializeOwned>(client: &'a Client, uri: String, query: Vec<(String, String)>) -> impl futures::stream::Stream<Item = Result<T, Error>> + 'a {
-    futures::stream::try_unfold(None, move |cursor| {
+    futures::stream::try_unfold(Cursor::Start, move |cursor| {
         let uri_clone = uri.clone();
         let query_clone = query.clone();
         async move {
-            let params = query_clone.into_iter().chain(cursor.map(|cursor| vec![(format!("after"), cursor)]).unwrap_or_default());
+            let query = if let Some(query) = cursor.query() {
+                query
+            } else {
+                return Ok(None); // Cursor::End
+            };
+            let params = query_clone.into_iter().chain(query);
             let PaginatedResult { data, pagination }: PaginatedResult<T> = client.get_raw(&uri_clone, params).await?;
             if data.is_empty() {
                 Ok::<_, Error>(None)
             } else {
-                Ok(Some((futures::stream::iter(data.into_iter().map(Ok)), Some(pagination.cursor))))
+                Ok(Some((futures::stream::iter(data.into_iter().map(Ok)), pagination.cursor)))
             }
         }
     }).try_flatten()
